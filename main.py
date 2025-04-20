6ce19a7366552871fd85a610f3a32438cdd8f850
Yuri Ferreira <yurifronaldo.com>
2025-04-20 11:38:58 -0300

    feat: adiciona a configuração e iniciação do banco

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
+from database.database import create_db_and_tables
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    create_db_and_tables()
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 
 @app.get("/")
 async def root():
