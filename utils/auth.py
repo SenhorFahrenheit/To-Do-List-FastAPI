14bc292fd50a8ebccd34a4cdd0f4969b2bd39c90
Yuri Ferreira <yurif9027@gmail.com>
2025-04-21 16:32:14 -0300

    Feat: adiciona o arquivo inicial da autenticação

---
+++ utils/auth.py
@@ -0,0 +1,21 @@
+from datetime import datetime, timedelta
+from jose import JWTError, jwt
+
+# Chave secreta — mantenha em segredo
+SECRET_KEY = "vFn3Kz!8j@Yq9$Lx%Nr4Ue#Wd^B1Cg&TzHs6Xp*Aq+Mk7RbVcJs2PfEo"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+def criar_token(dados: dict):
+    dados_para_codificar = dados.copy()
+    expira = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    dados_para_codificar.update({"exp": expira})
+    token_jwt = jwt.encode(dados_para_codificar, SECRET_KEY, algorithm=ALGORITHM)
+    return token_jwt
+
+def verificar_token(token: str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload
+    except JWTError:
+        return None
