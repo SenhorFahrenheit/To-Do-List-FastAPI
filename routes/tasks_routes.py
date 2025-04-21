3480f36d7467bdac88235f0f58c72b9e1a2fcb19
Yuri Ferreira <yurifronaldo.com>
2025-04-21 09:36:01 -0300

    Feat: adiciona o endpoint de cadastro de usuário. Fixes #7

--- routes/tasks_routes.py
+++ routes/tasks_routes.py
@@ -1,12 +1,29 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from pydantic import BaseModel
+from sqlmodel import Session
+from database.model import User, Task
+from utils.functions import gerar_hash
+from database.database import get_session
 
-class User(BaseModel):
-    username: str
-    password: str
 
 router = APIRouter()
 
+class UserSignUp(BaseModel):
+    username: str
+    password: str
+
+
+def create_user(db: Session, userBaseModel: UserSignUp):
+    username = userBaseModel.username
+    password_hash = gerar_hash(userBaseModel.password)
+    user = User(username=username, password=password_hash)
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+    return user
+
+
 @router.post("/sign_up")
-def sign_up(user: User):
-    pass
+def sign_up(user: UserSignUp, db: Session = Depends(get_session)):
+    return create_user(db, user)
+
