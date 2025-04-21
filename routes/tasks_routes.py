6cb3e974393c8ef82110ed78b6f055b34935a294
Yuri Ferreira <yurif9027@gmail.com>
2025-04-21 16:24:17 -0300

    Feat: adiciona a feature de login. Fixes #8

--- routes/tasks_routes.py
+++ routes/tasks_routes.py
@@ -2,12 +2,17 @@ from fastapi import APIRouter, Depends
 from pydantic import BaseModel
 from sqlmodel import Session
 from database.model import User, Task
-from utils.functions import gerar_hash
+from utils.functions import gerar_hash, verificar_senha
 from database.database import get_session
-# testando novamente
+from sqlmodel import select
+
 router = APIRouter()
 
 class UserSignUp(BaseModel):
+    username: str
+    password: str
+
+class UserSignIn(BaseModel):
     username: str
     password: str
 
@@ -21,8 +26,26 @@ def create_user(db: Session, userBaseModel: UserSignUp):
     db.refresh(user)
     return user
 
+def authenticate_user(db: Session, userBaseModel: UserSignIn):
+    username = userBaseModel.username
+    password = userBaseModel.password
+
+    # Busca o usuário
+    statement = select(User).where(User.username == username)
+    result = db.exec(statement).first()
+
+    if result:
+        if verificar_senha(password, result.password):
+            return {"message": "Login bem-sucedido", "user": result.username}
+        else:
+            return {"error": "Username ou senha incorretos"}
+    return {"error": "Username ou senha incorretos"}
 
 @router.post("/sign_up")
 def sign_up(user: UserSignUp, db: Session = Depends(get_session)):
     return create_user(db, user)
+
+@router.post("/sign_in")
+def sign_in(user: UserSignIn, db: Session = Depends(get_session)):
+    return authenticate_user(db, user)
 
