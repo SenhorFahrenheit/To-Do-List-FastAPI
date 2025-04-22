083b8c10407a6d0bee729d646294640c6a313089
Yuri Ferreira <yurif9027@gmail.com>
2025-04-22 17:30:59 -0300

    Feat: adiciona a rota de atualizar e sua proteção. Fixes #3

--- routes/tasks_routes.py
+++ routes/tasks_routes.py
@@ -5,6 +5,9 @@ from sqlmodel import Session, select
 from database.model import Task
 from database.database import get_session
 from utils.auth import verificar_token
+from typing import Optional
+
+
 
 router = APIRouter()
 
@@ -13,7 +16,9 @@ class TaskModel(BaseModel):
     description: str
     id_user: int
 
-
+class TaskUpdateModel(BaseModel):
+    title: Optional[str] = None
+    description: Optional[str] = None
 
 def create_task(db: Session, task: TaskModel):
     new_task = Task(
@@ -32,7 +37,7 @@ def get_tasks(db: Session, token):
     statement = select(Task).where(Task.id_user == user_id)
     tasks = db.exec(statement).all()
     # Mesmo sem tarefas, retorne lista vazia com status 200
-    return JSONResponse(content=tasks, status_code=200)
+    return JSONResponse(content=[task.model_dump() for task in tasks], status_code=200)
 
 def remove_task(db: Session, token, task_id):
     user_id = int(token["sub"])
@@ -48,6 +53,24 @@ def remove_task(db: Session, token, task_id):
     message = {"message": "Tarefa excluída com sucesso"}
 
     return message
+
+def update_task(db: Session, token, task_id, taskupdate: TaskUpdateModel):
+    user_id = int(token['sub'])
+    statement = select(Task).where(Task.id_user == user_id, Task.id == task_id)
+    task = db.exec(statement).first()
+
+    if not task:
+        raise HTTPException(status_code=404, detail="Tarefa não encontrada ou não pertence ao usuário")
+    
+    if taskupdate.title is not None:
+        task.title = taskupdate.title
+    if taskupdate.description is not None:
+        task.description = taskupdate.description
+
+    db.commit()
+    db.refresh(task)
+
+    return {"message": "Tarefa atualizada com sucesso"}
 
 @router.post("/add_task")
 def add_task(
@@ -82,3 +105,17 @@ def delete_tasks(
 
 
     return remove_task(db, token_valid, task_id)
+
+@router.put("/tasks/{task_id}")
+def put_tasks(
+    taskupdate: TaskUpdateModel,
+    task_id: int,
+    db: Session = Depends(get_session),
+    token_valid: str = Depends(verificar_token)
+   
+):
+    if token_valid is None:
+        raise HTTPException(status_code=401, detail="Token inválido ou ausente")     
+
+
+    return update_task(db, token_valid, task_id, taskupdate)
